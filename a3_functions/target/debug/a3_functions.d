e6fa39cc9bee754eaa52a15cf8ea4d15a34048f5
tusharviradiya <tusharviradiya2@gmail.com>
2024-04-04 16:10:29 +0530

    target file are deleted

--- a3_functions/target/debug/a3_functions.d
+++
@@ -1,1 +0,0 @@
-/var/www/html/learning/practice-with-rust/a3_functions/target/debug/a3_functions: /var/www/html/learning/practice-with-rust/a3_functions/src/main.rs
--- a3_functions/target/debug/deps/a3_functions-a45576c43f62281a.d
+++
@@ -1,5 +0,0 @@
-/var/www/html/learning/practice-with-rust/a3_functions/target/debug/deps/a3_functions-a45576c43f62281a: src/main.rs
-
-/var/www/html/learning/practice-with-rust/a3_functions/target/debug/deps/a3_functions-a45576c43f62281a.d: src/main.rs
-
-src/main.rs:
