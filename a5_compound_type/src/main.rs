6d40f286ec0b7495bfef49b1f529baa4041e0b13
tusharviradiya <tusharviradiya2@gmail.com>
2024-04-05 13:49:10 +0530

    compound type is completed

--- a5_compound_type/src/main.rs
+++ a5_compound_type/src/main.rs
@@ -1,3 +1,29 @@
+// // Fix error without adding new line
+// fn main() {
+//     let s: &str = "hello, world";
+
+//     println!("Success!");
+// }
+
+
+// Fix the error with at least two solutions
+// fn main() {
+//     let s: Box<str> = "hello, world".into();
+//     greetings(&s)
+// }
+
+// fn greetings(s: &str) {
+//     println!("{}",s)
+// }
+
+
+// Fill the blank
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("");
+    s.push_str("hello, world");
+    s.push('!');
+
+    assert_eq!(s, "hello, world!");
+
+    println!("Success!");
 }
