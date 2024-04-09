e03b28963817a3c9be07430bcddb683376d638ba
tusharviradiya <tusharviradiya2@gmail.com>
2024-04-09 15:43:10 +0530

    generics are copleted

--- a8_generics_traits/src/main.rs
+++ a8_generics_traits/src/main.rs
@@ -1,3 +1,23 @@
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T{
+    let mut largest = &list[0];
+    for item in list{
+        if item > largest{
+            largest = item;
+        }
+    }
+    largest
+}
+
+struct Point<T, U>{
+    x: T,
+    y: U,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut list = vec![21, 53, 4, 6, 8, 10, 12, 14, 16];
+    let result = largest(&list);
+    println!("largest number : {}", result);
+
+    let point = Point{x: 5, y: 10.12};
+    println!("x: {}, y: {}", point.x, point.y);
 }
