c2f265d62472c84581dde868ae52daf79ea12e23
tusharviradiya <tusharviradiya2@gmail.com>
2024-04-10 12:02:14 +0530

    generics and traits topic are completed

--- a8_generics_traits/src/main.rs
+++ a8_generics_traits/src/main.rs
@@ -1,23 +1,64 @@
-fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T{
-    let mut largest = &list[0];
-    for item in list{
-        if item > largest{
-            largest = item;
-        }
-    }
-    largest
+// fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T{
+//     let mut largest = &list[0];
+//     for item in list{
+//         if item > largest{
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
+
+// struct Point<T, U>{
+//     x: T,
+//     y: U,
+// }
+
+// fn main() {
+//     let mut list = vec![21, 53, 4, 6, 8, 10, 12, 14, 16];
+//     let result = largest(&list);
+//     println!("largest number : {}", result);
+
+//     let point = Point{x: 5, y: 10.12};
+//     println!("x: {}, y: {}", point.x, point.y);
+// }
+
+pub trait Summery{
+    fn summary(&self) -> String;
 }
 
-struct Point<T, U>{
-    x: T,
-    y: U,
+pub struct NewsArticle{
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}  
+
+impl Summery for NewsArticle{
+    fn summary(&self) -> String{
+        format!("{} by {} and {}", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet{
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summery for Tweet{
+    fn summary(&self) -> String{
+        format!("{}: {}", self.username, self.content)
+    }
+    
 }
 
 fn main() {
-    let mut list = vec![21, 53, 4, 6, 8, 10, 12, 14, 16];
-    let result = largest(&list);
-    println!("largest number : {}", result);
-
-    let point = Point{x: 5, y: 10.12};
-    println!("x: {}, y: {}", point.x, point.y);
+    let tweet = Tweet{
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false
+    };
+    println!("{}", tweet.summary());
 }
